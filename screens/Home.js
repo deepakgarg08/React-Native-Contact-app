e096a3ce438bdf86f11088e7d9df4cae778490d7
Deepak <erdkgarg08@gmail.com>
2020-08-20 13:23:50 +0200

    using map to create multiple list

--- screens/Home.js
+++ screens/Home.js
@@ -3,24 +3,45 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { Card } from 'react-native-paper'
 
 const Home = () => {
-    return (
-        <Card style={styles.mycard}>
-            <View style={styles.cardview}>
-                <Image
 
-                    style={styles.image}
-                    source={{ uri: "https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" }}
+    const data = [
+        { id: 1, name: 'Deepak', position: 'web developer' },
+        { id: 2, name: 'Vikas', position: 'Accounting' },
+        { id: 3, name: 'Aamir', position: 'goli developer' },
+        { id: 4, name: 'Hardeep', position: 'yoga developer' },
+        { id: 5, name: 'Sharique', position: 'noob developer' }
+    ]
 
-                />
-                <View style={styles.text}>
-                    <Text>Sexy Maama</Text>
-                    <Text>Software Developer</Text>
+    const renderList = data.map(item => {
+        return (
+            <Card style={styles.mycard} key = {item.id}>
+                <View style={styles.cardview}>
+                    <Image
+
+                        style={styles.image}
+                        source={{ uri: "https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" }}
+
+                    />
+                    <View style={styles.text}>
+                        <Text>{item.name}</Text>
+                        <Text>{item.position}</Text>
+                    </View>
                 </View>
-            </View>
 
-        </Card>
+            </Card>
+        )
+    })
+
+
+    return (
+        <View>
+            {renderList}
+        </View>
+
+
     )
 }
+
 
 const styles = StyleSheet.create({
     mycard: {
@@ -30,12 +51,12 @@ const styles = StyleSheet.create({
     image: {
         height: 60,
         width: 60,
-        borderRadius:30
+        borderRadius: 30
     },
     text: {
-        marginLeft:10,
-        marginTop:5,
-        fontSize:20
+        marginLeft: 10,
+        marginTop: 5,
+        fontSize: 20
     },
     cardview: {
         flexDirection: "row"
