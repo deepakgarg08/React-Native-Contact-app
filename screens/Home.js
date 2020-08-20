c18766da9b07d0a321885f717e269a10781b32fe
Deepak <erdkgarg08@gmail.com>
2020-08-20 13:14:12 +0200

    ist component card build with userphoto and name and description

--- screens/Home.js
+++ screens/Home.js
@@ -1,20 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { Card } from 'react-native-paper'
 
 const Home = () => {
     return (
-        <Card style = {styles.mycard}>
-            <Text>hello from home</Text>
+        <Card style={styles.mycard}>
+            <View style={styles.cardview}>
+                <Image
+
+                    style={styles.image}
+                    source={{ uri: "https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" }}
+
+                />
+                <View style={styles.text}>
+                    <Text>Sexy Maama</Text>
+                    <Text>Software Developer</Text>
+                </View>
+            </View>
+
         </Card>
     )
 }
 
-const styles =  StyleSheet.create({
+const styles = StyleSheet.create({
     mycard: {
-        margin:5,
+        margin: 5,
         padding: 5
+    },
+    image: {
+        height: 60,
+        width: 60,
+        borderRadius:30
+    },
+    text: {
+        marginLeft:10,
+        marginTop:5,
+        fontSize:20
+    },
+    cardview: {
+        flexDirection: "row"
+
     }
+
 })
 
 export default Home
